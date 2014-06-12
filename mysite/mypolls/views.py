99961e660cb214fc15c375f7c526cec9ec0fc882
leggo <acorley@gmx.de>
2014-06-12 21:46:13 +0200

    after watching a little bit of the tracy video
    what did I say?

--- mysite/mypolls/views.py
+++ mysite/mypolls/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, render_to_response
 
 def home(request):
+	number = 3
+	object_name = "Objecto Name"
 	
-    return render_to_response('index.html')
+	return render_to_response('index.html', {
+		'number': number,
+		'object_name': object_name,
+	})
+#    return render_to_response('index.html')
