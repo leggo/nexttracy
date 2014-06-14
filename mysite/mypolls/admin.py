c2446afd12042762b7d680526dbb9d3148c2e7a2
leggo <acorley@gmx.de>
2014-06-14 17:31:15 +0200

    revert to when this shit worked
    fasdfaf

--- mysite/mypolls/admin.py
+++ mysite/mypolls/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+#from mysite.mypolls.models import Object
+
 # Register your models here.
+#class ObjectAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description', 'created', 'slug',)
+	prepopulated_fields = {'slug': ('name',)}
+
+#admin.site.register(Object, ObjectAdmin)
